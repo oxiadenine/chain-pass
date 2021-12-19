03a4ade69f950525d9f68e6ea20120faaad1eb84
samgarasx <samgarasx@gmail.com>
2021-12-19 11:34:06 +0100

    Update Kotlin version to v1.5.31

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.compose.compose
 
 plugins {
-    kotlin("multiplatform")
-    kotlin("plugin.serialization")
     id("com.android.library")
+    kotlin("multiplatform")
     id("org.jetbrains.compose")
+    kotlin("plugin.serialization")
 }
 
 kotlin {
