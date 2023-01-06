44333198d646f0081346d6ddc846158f3723e93b
samgarasx <samgarasx@gmail.com>
2023-01-06 11:49:56 +0100

    Update compose version to v1.2.2

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -74,11 +74,11 @@ kotlin {
 }
 
 android {
-    compileSdk = 32
+    compileSdk = 33
 
     defaultConfig {
         minSdk = 26
-        targetSdk = 32
+        targetSdk = 33
     }
 
     compileOptions {
