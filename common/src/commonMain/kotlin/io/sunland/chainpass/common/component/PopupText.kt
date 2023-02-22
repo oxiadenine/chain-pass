c13230c93a457b96f24151b01abc54c734baba0a
samgarasx <samgarasx@gmail.com>
2023-02-22 17:28:48 +0100

    Fix ChainLink password copied popup position

--- common/src/commonMain/kotlin/io/sunland/chainpass/common/component/PopupText.kt
+++
@@ -1,8 +0,0 @@
-package io.sunland.chainpass.common.component
-
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.unit.IntOffset
-
-@Composable
-expect fun PopupText(alignment: Alignment, offset: IntOffset = IntOffset.Zero, text: String)
