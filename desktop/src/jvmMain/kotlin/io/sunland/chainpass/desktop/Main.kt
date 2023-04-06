fc1fa04fbcc05ea58ea78cacf4a5ebf37f9cdde8
samgarasx <samgarasx@gmail.com>
2023-04-06 12:01:18 +0200

    Update Desktop window initial size and position

--- desktop/src/jvmMain/kotlin/io/sunland/chainpass/desktop/Main.kt
+++ desktop/src/jvmMain/kotlin/io/sunland/chainpass/desktop/Main.kt
@@ -3,9 +3,14 @@ package io.sunland.chainpass.desktop
 import androidx.compose.foundation.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.DpSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import io.sunland.chainpass.common.*
 import io.sunland.chainpass.common.component.rememberNavigationState
 import io.sunland.chainpass.common.network.*
@@ -24,10 +29,16 @@ fun main() {
     val syncServer = SyncServer(chainRepository, chainLinkRepository).start()
 
     application {
+        val windowState = rememberWindowState()
+
+        windowState.size = DpSize(640.dp, 480.dp)
+        windowState.position = WindowPosition(Alignment.Center)
+
         Window(
-            icon = painterResource("icon.png"),
+            state = windowState,
+            onCloseRequest = ::exitApplication,
             title = "Chain Pass",
-            onCloseRequest = ::exitApplication
+            icon = painterResource("icon.png")
         ) {
             window.minimumSize = Dimension(360, 480)
 
