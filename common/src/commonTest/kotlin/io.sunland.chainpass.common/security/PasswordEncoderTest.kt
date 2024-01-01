087e6c58c76ac34677b4b1f3d053c7a382f21865
oxiadenine <sga.samuel@gmail.com>
2024-01-01 13:57:42 +0100

    Fix PasswordEncoderTest

--- common/src/commonTest/kotlin/io.sunland.chainpass.common/security/PasswordEncoderTest.kt
+++ common/src/commonTest/kotlin/io.sunland.chainpass.common/security/PasswordEncoderTest.kt
@@ -6,10 +6,10 @@ import kotlin.test.assertEquals
 
 @AndroidIgnore
 class PasswordEncoderTest {
-    private val hash = "o9N4Ue1+qSP7ztU40OB69iHJCpNDU+iX9bQIMRdEkYk="
+    private val hash = "yOWTvJt/djLbFr+Caj/7SieMbQp0xR+cskIeNHzwsZQ="
 
     @Test
-    fun hashTest() {
+    fun testHash() {
         assertEquals(hash, PasswordEncoder.hash(EncoderSpec.Passphrase(
             PasswordEncoder.Base64.encode("test".encodeToByteArray()),
             PasswordEncoder.Base64.encode("test".encodeToByteArray())
@@ -17,13 +17,16 @@ class PasswordEncoderTest {
     }
 
     @Test
-    fun encryptDecryptTest() {
+    fun testEncryptDecrypt() {
         val encryptedPassword = PasswordEncoder.encrypt(
-            EncoderSpec.Passphrase(hash, hash),
-            PasswordEncoder.Base64.encode("test".encodeToByteArray())
+            PasswordEncoder.Base64.encode("test".encodeToByteArray()),
+            EncoderSpec.Passphrase(hash, PasswordEncoder.Base64.encode("test".encodeToByteArray()))
         )
 
-        val plainPassword = PasswordEncoder.decrypt(EncoderSpec.Passphrase(hash, hash), encryptedPassword)
+        val plainPassword = PasswordEncoder.decrypt(
+            encryptedPassword,
+            EncoderSpec.Passphrase(hash, PasswordEncoder.Base64.encode("test".encodeToByteArray()))
+        )
 
         assertEquals("test", PasswordEncoder.Base64.decode(plainPassword).decodeToString())
     }
