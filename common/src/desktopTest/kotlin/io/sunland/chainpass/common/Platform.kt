abaafa32e27e3e37f5bc03baa9cb60dc1afaff2a
oxiadenine <sga.samuel@gmail.com>
2024-01-01 13:57:46 +0100

    Update Gradle wrapper, plugins and dependencies versions

--- common/src/desktopTest/kotlin/io/sunland/chainpass/common/Platform.kt
+++ common/src/desktopTest/kotlin/io/sunland/chainpass/common/Platform.kt
@@ -1,6 +1,6 @@
 package io.sunland.chainpass.common
 
-actual typealias DesktopIgnore = org.junit.Ignore
+actual typealias DesktopIgnore = org.junit.jupiter.api.Disabled
 
 @Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
 actual annotation class AndroidIgnore
