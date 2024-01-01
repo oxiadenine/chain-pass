b0412eefb8018fde44ce99945b81acff399f71d4
oxiadenine <sga.samuel@gmail.com>
2024-01-01 13:57:46 +0100

    Add ModalDrawer

--- common/src/commonMain/kotlin/io/sunland/chainpass/common/App.kt
+++ common/src/commonMain/kotlin/io/sunland/chainpass/common/App.kt
@@ -1,13 +1,28 @@
 package io.sunland.chainpass.common
 
 import androidx.compose.animation.*
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.input.pointer.PointerIconDefaults
+import androidx.compose.ui.input.pointer.pointerHoverIcon
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import io.rsocket.kotlin.RSocketRequestHandler
 import io.rsocket.kotlin.payload.Payload
 import io.sunland.chainpass.common.component.rememberScaffoldListState
@@ -67,6 +82,7 @@ fun rememberSettingsState(settingsManager: SettingsManager) = remember {
     )
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun App(settingsManager: SettingsManager, database: Database, storage: Storage) = MaterialTheme(
     colors = darkColors(
@@ -97,6 +113,7 @@ fun App(settingsManager: SettingsManager, database: Database, storage: Storage) 
 
     val navigationState = rememberNavigationState(Screen.CHAIN_LIST)
     val settingsState = rememberSettingsState(settingsManager)
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
 
     coroutineScope.launch(Dispatchers.IO) {
         WebSocket.start(WebSocket.getLocalHost()) {
@@ -111,59 +128,102 @@ fun App(settingsManager: SettingsManager, database: Database, storage: Storage) 
         }
     }
 
-    Surface(modifier = Modifier.fillMaxSize()) {
-        val hostAddressState = remember { mutableStateOf("") }
+    val hostAddressState = remember { mutableStateOf("") }
 
+    ModalDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            Column(
+                modifier = Modifier.fillMaxWidth().padding(all = 16.dp),
+                verticalArrangement = Arrangement.spacedBy(space = 4.dp)
+            ) {
+                Text(text = platform)
+                Text(text = hostAddressState.value.ifEmpty { "No internet access" }, fontSize = 14.sp)
+            }
+            Column(modifier = Modifier.fillMaxWidth()) {
+                Row(
+                    modifier = Modifier.fillMaxWidth().clickable {
+                        navigationState.screenState.value = Screen.SETTINGS
+
+                        coroutineScope.launch {
+                            drawerState.close()
+                        }
+                    }.pointerHoverIcon(icon = PointerIconDefaults.Hand),
+                    horizontalArrangement = Arrangement.spacedBy(space = 16.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        modifier = Modifier.padding(start = 16.dp),
+                        imageVector = Icons.Default.Settings,
+                        contentDescription = null
+                    )
+                    Text(text = "Settings", modifier = Modifier.padding(vertical = 16.dp), fontSize = 14.sp)
+                }
+            }
+        },
+        drawerShape = object : Shape {
+            override fun createOutline(size: Size, layoutDirection: LayoutDirection, density: Density): Outline {
+                return with(density) {
+                    Outline.Rectangle(Rect(0f,0f, 300.dp.roundToPx().toFloat(), size.height))
+                }
+            }
+        },
+        drawerBackgroundColor = MaterialTheme.colors.surface,
+        scrimColor = Color.Black.copy(alpha = 0.5f)
+    ) {
         LaunchedEffect(hostAddressState.value) {
             hostAddressState.value = WebSocket.getLocalHost()
         }
 
-        Crossfade(targetState = navigationState.screenState.value) { screen ->
-            when (screen) {
-                Screen.SETTINGS -> {
-                    Settings(
-                        hostAddress = hostAddressState.value,
-                        storePath = storage.storePath,
-                        settingsState = settingsState,
-                        navigationState = navigationState,
-                        modifier = Modifier.fillMaxSize()
-                    )
-                }
-                Screen.CHAIN_LIST -> {
-                    val chainListViewModel = ChainListViewModel(
-                        chainRepository,
-                        chainLinkRepository,
-                        PasswordGenerator(PasswordGenerator.Strength(
-                            settingsState.passwordLengthState.value,
-                            settingsState.passwordIsAlphanumericState.value
-                        )),
-                        storage
-                    )
-
-                    val scaffoldListState = rememberScaffoldListState()
-
-                    ChainScaffoldList(
-                        viewModel = chainListViewModel,
-                        settingsState = settingsState,
-                        navigationState = navigationState,
-                        scaffoldListState = scaffoldListState,
-                        modifier = Modifier.fillMaxSize()
-                    )
-                }
-                Screen.CHAIN_LINK_LIST -> {
-                    val chainLinkListViewModel = ChainLinkListViewModel(chainLinkRepository, storage).apply {
-                        chain = navigationState.chainState.value
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Crossfade(targetState = navigationState.screenState.value) { screen ->
+                when (screen) {
+                    Screen.SETTINGS -> {
+                        Settings(
+                            hostAddress = hostAddressState.value,
+                            storePath = storage.storePath,
+                            settingsState = settingsState,
+                            navigationState = navigationState,
+                            modifier = Modifier.fillMaxSize()
+                        )
                     }
+                    Screen.CHAIN_LIST -> {
+                        val chainListViewModel = ChainListViewModel(
+                            chainRepository,
+                            chainLinkRepository,
+                            PasswordGenerator(PasswordGenerator.Strength(
+                                settingsState.passwordLengthState.value,
+                                settingsState.passwordIsAlphanumericState.value
+                            )),
+                            storage
+                        )
 
-                    val scaffoldListState = rememberScaffoldListState()
+                        val scaffoldListState = rememberScaffoldListState()
 
-                    ChainLinkScaffoldList(
-                        viewModel = chainLinkListViewModel,
-                        settingsState = settingsState,
-                        navigationState = navigationState,
-                        scaffoldListState = scaffoldListState,
-                        modifier = Modifier.fillMaxSize()
-                    )
+                        ChainScaffoldList(
+                            viewModel = chainListViewModel,
+                            settingsState = settingsState,
+                            navigationState = navigationState,
+                            drawerState = drawerState,
+                            scaffoldListState = scaffoldListState,
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    }
+                    Screen.CHAIN_LINK_LIST -> {
+                        val chainLinkListViewModel = ChainLinkListViewModel(chainLinkRepository, storage).apply {
+                            chain = navigationState.chainState.value
+                        }
+
+                        val scaffoldListState = rememberScaffoldListState()
+
+                        ChainLinkScaffoldList(
+                            viewModel = chainLinkListViewModel,
+                            settingsState = settingsState,
+                            navigationState = navigationState,
+                            scaffoldListState = scaffoldListState,
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    }
                 }
             }
         }
---
+++ common/src/commonMain/kotlin/io/sunland/chainpass/common/Platform.kt
@@ -0,0 +1,3 @@
+package io.sunland.chainpass.common
+
+expect val platform: String
